b4461fb38f1789a8bdd1b3a3b79adffa7da359e7
premprakashsingh <premsingh.singh386@gmail.com>
2017-07-22 03:45:31 +0530

    Added Oauth for add to slack button and did some code formatting

--- server.js
+++ server.js
@@ -6,40 +6,38 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var request = require('request');
-var moment = require('moment')
-// we've started you off with Express, 
+var moment = require('moment');
+// we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
-// http://expressjs.com/en/starter/basic-routing.html
+
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.post('/meetupbot', function(req, res){
-  //console.log(req.headers);
-  console.log(JSON.stringify(req.body));
+app.post('/meetupbot', function (req, res) {
+  /* /meetupbot calls the meetupbot and response is list of commands */
   var userName = req.body.user_name;
   var reply = {
-    "text": "Hello, "+userName+" I am a MeetupBot. I show list of meetups going on near your location.\n Try following commands :", 
+    "text": "Hello, " +userName+ " I am a MeetupBot. I show list of meetups going on near your location.\n Try following commands :",
     "attachments": [
-        {
-            title: "1) /meetupbot-find <location> (& <group>)",
-            text: "use this to find local meetup-groups based on your location \nfor ex: /meetupbot-find New-York"
-            + "\noptional: add parameter for your interests: '/meetupbot-find New-York & <interest>'",
-            color: "#764FA5"
-        },
-        {
-        title: "2) /meetupbot-show <location> & <interest>", 
-        text: "use this to find events based on location and your interests \nfor ex: /meetupbot-show Mumbai & Javascript (Don't forget to use ampersand (&).)",
-        color: "#764FA5"
-        }
-        ]
+       {
+         title: "1) /meetupbot-find <location> (& <group>)",
+         text: `use this to find local meetup-groups based on your location \nfor ex: /meetupbot-find New-York
+         \noptional: add parameter for your interests: '/meetupbot-find New-York & <interest>'`,
+         color: "#764FA5"
+       },
+       {
+         title: "2) /meetupbot-show <location> & <interest>",
+         text: "use this to find events based on location and your interests \nfor ex: /meetupbot-show Mumbai & Javascript (Don't forget to use ampersand (&).)",
+         color: "#764FA5"
+       }
+    ]
   };
   res.json(reply);
-  
 });
 
 app.post('/meetupbot-show', function(req, res) {
@@ -48,10 +46,10 @@ app.post('/meetupbot-show', function(req, res) {
   var commandText = req.body.text;
   var area = commandText.split('&')[0];
   var interest = commandText.split('&')[1];
-  console.log("command is "+commandText);
   var reply = {};
   var location={};
   var attachment=[];
+
   if(!commandText || commandText == undefined){
     reply.text = 'Please provide a location along with the command @'+userName+'\nFor ex: /meetupbot-show Mumbai & Javascript.';
     return res.json({text: reply.text});
@@ -72,7 +70,6 @@ app.post('/meetupbot-show', function(req, res) {
              }
              events.forEach((event) => {
                var status = (event.status != undefined) ? ('Status - '+event.status) : 'Status - visible only to Members';
-               var desc = event.description ? event.description.split('</p>')[0] :  "Description - Only visible to members.";
                var date = new Date(event.time + event.utc_offset);
                date = moment(date).format('lll');
                var venue = (event.venue != undefined) ? event.venue.address_1 : 'Only visible to members';
@@ -83,22 +80,10 @@ app.post('/meetupbot-show', function(req, res) {
                  title_link: 'https://www.meetup.com/'+event.group.urlname,
                  color: "#764FA5",
                  fields: [
-                      {
-                          "title": "Date",
-                          "value": date,
-                          "short": true
-                      },
-                      {
-                          "title": "Venue",
-                          "value": venue,
-                          "short": true
-                      },
-                      {
-                          "title": "RSVP Count",
-                          "value": event.yes_rsvp_count,
-                          "short": true
-                      }
-                ]
+                   { "title": "Date", "value": date, "short": true },
+                   { "title": "Venue", "value": venue, "short": true },
+                   { "title": "RSVP Count", "value": event.yes_rsvp_count, "short": true }
+                 ]
                });
              });
              reply.attachments = attachment;
@@ -112,70 +97,70 @@ app.post('/meetupbot-show', function(req, res) {
       .catch((e) => {
          console.log("error in geocode as "+e);
          return res.json({text: 'Ops something went wrong. Please try again :blush:'});
-       }); 
+       });
   }
 });
 
-app.post('/meetupbot-find', function(req, res){
+app.post('/meetupbot-find', function(req, res) {
     var location = {};
     var commandText = req.body.text.replace(" ", "");
-    if(commandText.includes("&")) {
-    var area = commandText.split("&")[0];
-    var interest = commandText.split("&")[1];
-    } else {
-      var area = commandText;
-      var interest = "";
-    }
     var userName = req.body.user_name;
     var reply = {};
-    
-    if(!commandText || commandText == undefined){
-        reply.text = '@' + userName + ' Please provide a location along with the command \nFor ex: /meetupbot-find London'
-        + ' (& <category>)';
-        return res.json(reply);
-    } else if(interest === undefined) {
-        reply.text = '@' + userName + ' Please use the correct syntax: /meetupbot-find <location> & <interest>';
-        return res.json(reply);
+    var area,interest;
+
+    if(commandText.includes("&")) {
+      area = commandText.split("&")[0];
+      interest = commandText.split("&")[1];
     } else {
-        reply.text = "Hey @" + userName +"\nThose are the groups near " + area;
-        if(interest) reply.text += " for" + interest;
-        
-        getGeoCode(area)
+      area = commandText;
+      interest = "";
+    }
+
+    if(!commandText || commandText == undefined){
+      reply.text = '@' + userName + ` Please provide a location along with the command \nFor ex: /meetupbot-find London
+       (& <category>)`;
+      return res.json(reply);
+    } else if(interest === undefined) {
+      reply.text = '@' + userName + ' Please use the correct syntax: /meetupbot-find <location> & <interest>';
+      return res.json(reply);
+    } else {
+      reply.text = "Hey @" + userName +"\nThose are the groups near " + area;
+      if(interest) reply.text += " for" + interest;
+
+      getGeoCode(area)
         .then(function(data) {
-            location.lat = data.lat;
-            location.lon = data.lng;
-            
-            findMeetupGroups(location, interest)
+          location.lat = data.lat;
+          location.lon = data.lng;
+
+          findMeetupGroups(location, interest)
             .then(function(groups) {
-                if (groups.length === 0) {
-                    reply.text = 'No groups found near '+area+' :sleuth_or_spy: .\nMake sure the location you entered is correct ' 
-                    + 'and you didn\'t forget the "&". \nPlease  try again.:slightly_smiling_face:';
-                    return res.json(reply);
-                } else if (groups.length > 20) {
-                    reply.attachments = [];
-                    for (var i = 0; i <21; i++) {
-                        var current = groups[i];
-                        composeAttachments(reply.attachments, current);
-                    };
-                    return res.json(reply);
-                } else {
-                    reply.attachments = [];
-                    groups.forEach(function(group) {
-                        composeAttachments(reply.attachments, group);
-                    });
-                    return res.json(reply);
+              if (groups.length === 0) {
+                reply.text = 'No groups found near '+area+` :sleuth_or_spy: .\nMake sure the location you entered is correct
+                and you didn\'t forget the "&". \nPlease  try again.:slightly_smiling_face:`;
+                return res.json(reply);
+              } else if (groups.length > 20) {
+                reply.attachments = [];
+                for (var i = 0; i <21; i++) {
+                  var current = groups[i];
+                  composeAttachments(reply.attachments, current);
                 }
-                
+                return res.json(reply);
+              } else {
+                reply.attachments = [];
+                groups.forEach(function(group) {
+                    composeAttachments(reply.attachments, group);
+                });
+                return res.json(reply);
+              }
             })
             .catch(function() { //catch for findMeetupGroups
-                return res.json({text: 'Ops something went wrong. Please try again :blush:'});
-            })
-        })
-        .catch(function() { // catch for getGeoCode
-            return res.json({text: 'Ops something went wrong. Please try again :blush:'});
-        });
+              return res.json({text: 'Ops something went wrong. Please try again :blush:'});
+            });
+      })
+      .catch(function() { // catch for getGeoCode
+          return res.json({text: 'Ops something went wrong. Please try again :blush:'});
+      });
     }
-    
 });
 
 /*
@@ -193,8 +178,7 @@ function getGeoCode(location){
       if (error) {
         console.log("Error occured in getGeoCode as "+error);
         reject();
-      }
-      else{
+      } else {
         body = JSON.parse(body);
         var loc = body.results[0].geometry.location;
         resolve(loc);
@@ -207,7 +191,7 @@ function getGeoCode(location){
 */
 function getMeetupEvents(location, interest) {
   var key = process.env.SECRET;
-  
+
   return new Promise((resolve, reject) => {
     var options = { method: 'GET',
       url: 'https://api.meetup.com/find/events',
@@ -225,86 +209,118 @@ function getMeetupEvents(location, interest) {
       if (error) {
         console.log("error occured in getMeetupEvents as "+error);
         reject();
-      } else{
+      } else {
         body = JSON.parse(body);
         console.log(body.length);
         resolve(body);
       }
     });
   });
-};
+}
 
 /*
 *function to get meetup-groups near your city/town/location using meetup API
 */
-function findMeetupGroups (location, interest) {
+function findMeetupGroups (location, interest){
     var key = process.env.SECRET;
-    
-    return new Promise(function(resolve, reject) {
-        var options = {
-            method: "GET",
-            url: "https://api.meetup.com/find/groups",
-            qs: {
-                key: key,
-                lat: location.lat,
-                lon: location.lon,
-                radius: 10
-            }
+
+    return new Promise(function(resolve, reject){
+      var options = {
+        method: "GET",
+        url: "https://api.meetup.com/find/groups",
+        qs: {
+          key: key,
+          lat: location.lat,
+          lon: location.lon,
+          radius: 10
         }
-        if(!(interest === undefined || interest === "")) options.qs.text = interest;
-        console.log(options);
-        
-        //api-request
-        request(options, function(error, response, body) {
-           if(error) {
-               console.log("error occured in findMeetupGroups: " + error);
-               reject
-           } else {
-               body = JSON.parse(body);
-              // console.log(response);
-               resolve(body);
-           }
-        }); 
+      };
+      if(!(interest === undefined || interest === "")) options.qs.text = interest;
+
+      //api-request
+      request(options, function(error, response, body) {
+        if(error) {
+          console.log("error occured in findMeetupGroups: " + error);
+          reject();
+         } else {
+           body = JSON.parse(body);
+           resolve(body);
+         }
+      });
     });
-};
+}
 
 //function to help compose reply
 function composeAttachments(arr, obj){
-    arr.push({
-                            title: obj.name,
-                            text: /*obj.description,*/removeHtml(obj.description),
-                            color: "#764FA5",
-                            mrkdwn_in: ["text", "attachments"],
-                            fields: [
-                                {
-                                    "title": "Link",
-                                    "value": obj.link,
-                                    "short": true
-                                },
-                                {
-                                    "title": "Members",
-                                    "value": obj.members,
-                                    "short": true
-                                },
-                            ]
-                        })
-};
+  arr.push({
+    title: obj.name,
+    text: /*obj.description,*/removeHtml(obj.description),
+    color: "#764FA5",
+    mrkdwn_in: ["text", "attachments"],
+    fields: [
+      {"title": "Link","value": obj.link,"short": true},
+      {"title": "Members","value": obj.members,"short": true},
+    ]
+  });
+}
 
 function removeHtml(str) {
-    var tags = /<\/?\w+>/g, bold = /<\/?b>/g, entities = /&\w+;/g, italics = /<\/?i>/g;
-    var linebreak = /<\/?br>/g, imgAndA = /<(?:a\s|img\s).+>/g;
-    var descr;
-    // cut str to 300 characters
-    if (str.length>300) descr=str.substr(0, 300);
-    else descr = str;
-    var description = descr.replace(bold, "*").replace(italics, "_").replace(entities, "").replace(linebreak, "\n");
-    var result = description.replace(tags, "").replace(imgAndA, "").replace(/<\w*$/, "");
-    return result + "...";
-};
+  var tags = /<\/?\w+>/g, bold = /<\/?b>/g, entities = /&\w+;/g, italics = /<\/?i>/g;
+  var linebreak = /<\/?br>/g, imgAndA = /<(?:a\s|img\s).+>/g;
+  var descr;
+  // cut str to 300 characters
+  if (str.length>300) descr=str.substr(0, 300);
+  else descr = str;
+  var description = descr.replace(bold, "*").replace(italics, "_").replace(entities, "").replace(linebreak, "\n");
+  var result = description.replace(tags, "").replace(imgAndA, "").replace(/<\w*$/, "");
+  return result + "...";
+}
 
+app.get('/redirected', function(req,res){
+  //route for redirection for the add to slack button
+  doOauth(req.query)
+    .then((data)=>{
+      //got the tooken i.e successfully installed the MeetupBot on slack channel.
+      console.log('doOauth then block with data as '+JSON.stringify(data));
+      res.redirect('/');
+    })
+    .catch((err)=>{
+     res.json({'error': 'Ops Something went wrong! Please try again.'});
+     console.log('error as '+err);
+    });
+});
+
+function doOauth(data){
+  return new Promise((resolve, reject) => {
+    var options = {
+      method: 'POST',
+      headers:{
+        'content-type': 'application/x-www-form-urlencoded',
+        charset: 'utf-8',
+      },
+      url: 'https://slack.com/api/oauth.access',
+      form: {
+        'client_id': process.env.slackClientId,
+        'client_secret': process.env.slackSecret,
+        'code': data.code
+      },
+      json: true,
+      'redirect_uri':'https://meetupbot.glitch.me/redirected'
+    };
+
+    console.log(options);
+    request(options, function (error, response, body) {
+      if (error) {
+        console.log("error occured in doOauth as "+error);
+        reject();
+      } else{
+        resolve(body);
+      }
+    });
+  });
+}
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
-
